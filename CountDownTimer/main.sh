5c67bb8650de7b18bcd2a2d7204351ee55f25ee5
NotYourAnonOne <104510802+NotYourAnonOne@users.noreply.github.com>
2023-08-23 16:35:41 +0330

    Create main.sh

---
+++ CountDownTimer/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to display countdown
+countdown() {
+    local remaining=$1
+
+    while [ $remaining -gt 0 ]; do
+        echo -ne "Remaining time: $remaining seconds\r"
+        sleep 1
+        ((remaining--))
+    done
+
+    echo -e "Time's up!"
+}
+
+# Check if duration is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 duration_in_seconds"
+    exit 1
+fi
+
+duration=$1
+countdown "$duration"
